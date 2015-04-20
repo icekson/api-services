9c95ba167d6b8cc1e7fa9db5dfd4b640ce86ef47
Alexey Itsekson <icekson85@gmail.com>
2015-04-20 13:46:55 +0000

    recreate and fixed XmlBuilder

--- src/Api/Service/Response/XmlBuilder.php
+++ src/Api/Service/Response/XmlBuilder.php
@@ -8,51 +8,77 @@ namespace Api\Service\Response;
 use Api\Service\Response\Builder as ResponseBuilder;
 
 class XmlBuilder extends ObjectBuilder {
+
 	private $xml = null;
 
 	public function result() {
-		$xml = new \XMLWriter ();
-		$this->xml = $xml;
-		$xml->openMemory ();
-		$xml->startElement ( "response" );
-		$xml->writeElement ( "status", $this->status );
-		$xml->startElement ( "messages" );
-		foreach ( $this->messages as $message ) {
-			$xml->writeElement ( "message", $message );
-		}
-		$xml->endElement ();
-		$data = ( array ) $this->data;
-		if (count ( $data ) > 0) {
-			$xml->startElement ( $this->rootElementName );
-			array_walk_recursive ( $data, array (
-					$this,
-					'addElement' 
-			) );
-		} else {
-			$xml->writeElement ( $this->rootElementName );
-		}
-		$xml->endElement ();
-		$xml->endElement ();
-		$res = $xml->outputMemory ( true );
-		
-		return '<?xml version="1.0" encoding="utf-8" ?>' . $res;
+        $msgs = array();
+        foreach($this->messages as $msg){
+            $m = array();
+            $m['message'] = $msg;
+        }
+		$arr = array(
+            'status' => $this->status,
+            'success' => !$this->isError(),
+            'messages' => $msgs,
+            'data' => (is_array($this->data) ? $this->data : array($this->data))
+        );
+        $res = $this->toXml($arr);
+        return $res;
 	}
-	private function addElement($val, $key) {
-		if (is_int ( $key ) && !is_string($val)) {
-			$this->xml->startElement ( "item" );
-			foreach ( ( array ) $val as $k => $v ) {
-				$k = preg_replace ( "/\W+/i", "", $k );
-				if (is_string ( $k ) && strlen ( $k ) > 0) {
-					$this->xml->writeElement ( $k, $v );
-				}
-			}
-			$this->xml->endElement ();
-		} else {
-			$key = preg_replace ( "/\W+/i", "", $key );
-			if (is_string ( $key ) && (int)$key !== 0 && strlen ( $key ) > 0) {
-				$this->xml->writeElement ( $key, $val );
-			}
-		}
-	}
+
+    /**
+     * The main function for converting to an XML document.
+     * Pass in a multi dimensional array and this recrusively loops through and builds up an XML document.
+     *
+     * @param array $data
+     * @param string $rootNodeName - what you want the root node to be - defaultsto data.
+     * @param SimpleXMLElement $xml - should only be used recursively
+     * @return string XML
+     */
+    private function toXml($data, $rootNodeName = 'response', $xml=null)
+    {
+        // turn off compatibility mode as simple xml throws a wobbly if you don't.
+        if (ini_get('zend.ze1_compatibility_mode') == 1)
+        {
+            ini_set ('zend.ze1_compatibility_mode', 0);
+        }
+
+        if ($xml == null)
+        {
+            $xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
+        }
+
+        // loop through the data passed in.
+        foreach($data as $key => $value)
+        {
+            // no numeric keys in our xml please!
+            if (is_numeric($key))
+            {
+                // make string key...
+                $key = "item"/*. (string) $key*/;
+            }
+
+            // replace anything not alpha numeric
+            $key = preg_replace('/[^a-z]/i', '', $key);
+
+            // if there is another array found recrusively call this function
+            if (is_array($value))
+            {
+                $node = $xml->addChild($key);
+                // recrusive call.
+                $this->toXml($value, $rootNodeName, $node);
+            }
+            else
+            {
+                // add single node.
+                $value = htmlentities($value);
+                $xml->addChild($key,$value);
+            }
+
+        }
+        // pass back as string. or simple xml object if you want!
+        return $xml->asXML();
+    }
 
 }
