36931a8438218df1fa01bbe4dd6bbec074c6f40b
unknown <icekson85@gmail.com>
2015-04-24 19:05:02 +0300

    *

--- src/Api/Service/AccessLoggerInterface.php
+++ src/Api/Service/AccessLoggerInterface.php
@@ -11,5 +11,11 @@ use Api\Service\Util\Properties;
 use Api\Service\Response\Builder as ResponseBuilder;
 
 interface AccessLoggerInterface {
+	
+	/**
+	* @param string $accessToken
+	* @param Properties $params
+	* @param ResponseBuilder $response	
+	**/
     public function log($accessToken, Properties $params, ResponseBuilder $response);
 } 
