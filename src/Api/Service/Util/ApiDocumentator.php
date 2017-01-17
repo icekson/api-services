c6b7db8046cf99821acfa63f7856ba61c3c25cc0
Alexey Itsekson <icekson@inbox.ru>
2017-01-17 16:15:42 +0200

    hot fix -  'added Input and Output annotations for service actions, added ApiDocumentator'

--- src/Api/Service/Util/ApiDocumentator.php
+++ src/Api/Service/Util/ApiDocumentator.php
@@ -125,16 +125,16 @@ class ApiDocumentator
 
                         }
                     }
+                    usort($res[$classAnn->name], function($a, $b){
+                        if($a["name"] > $b['name']){
+                            return 1;
+                        }else if($a["name"] < $b['name']){
+                            return -1;
+                        } else{
+                            return 0;
+                        }
+                    });
                 }
-                usort($res[$classAnn->name], function($a, $b){
-                    if($a["name"] > $b['name']){
-                        return 1;
-                    }else if($a["name"] < $b['name']){
-                        return -1;
-                    } else{
-                        return 0;
-                    }
-                });
             }
 
         }
